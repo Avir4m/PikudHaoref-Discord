6c24f34a8f8fe90de563077d35fa7dad92193a4a
Avir4m <avir4m@gmail.com>
2023-11-14 16:08:28 +0200

    configed tzeva-adom api

--- src/main.js
+++ src/main.js
@@ -1,5 +1,7 @@
 import { Client, GatewayIntentBits } from 'discord.js';
 import { config } from 'dotenv';
+import { getActiveAlert } from 'pikud-haoref-api';
+
 
 config();
 
@@ -10,4 +12,18 @@ client.on('ready', () => {
   console.log(`Logged in as ${client.user.tag}!`);
 });
 
+var poll = function () {
+    var options = {
+    };
+
+    getActiveAlert(function (err, alert) {
+        setTimeout(poll, 5000);
+        if (err) {
+            return console.log('Retrieving active alert failed: ', err);
+        }
+        console.log(alert);
+    }, options);
+}
 client.login(TOKEN);
+
+poll();

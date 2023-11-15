d649116a15dae65ee3f27478ddf37f6747e1d1db
Avir4m <avir4m@gmail.com>
2023-11-15 08:34:59 +0200

    add embed for prettier look

--- src/main.js
+++ src/main.js
@@ -1,4 +1,4 @@
-import { Client, Embed, GatewayIntentBits } from 'discord.js';
+import { Client, EmbedBuilder, GatewayIntentBits } from 'discord.js';
 import { config } from 'dotenv';
 import { getActiveAlert } from 'pikud-haoref-api';
 
@@ -20,19 +20,18 @@ const poll = () => {
         if (err) {
             console.error('Retrieving active alert failed: ', err);
         } else {
-            console.log(alert);
             const alertChannel = client.channels.cache.get('1173985945687687270');
+            const embed = new EmbedBuilder()
+            .setTitle(`התראות בזמן אמת`)
+            .setColor(`0xff0000`)
+            .setFields(
+              {name: `סוג התראה`, value: `${alert.type}`},
+              {name: `ערים`, value: `${alert.cities}`},
+              {name: `הוראות פיקוד העורף`, value: `${alert.instructions}`},
+              )
             if (alertChannel) {
                 if (alert.type != 'none') {
-                    const embed = new MessageEmbed()
-                    .setTitle('התראה בזמן אמת')
-                    .setColor('0xff0000')
-                    .addFields(
-                        {"name": `סוג התראה :`, "value": `${alert.type}`},
-                        {"name": `ערים`, "value": `${alert.cities}`},
-                        {"name": `הוראות פיקוד העורף`,"value": `${alert.instructions}`}
-                    )
-                    alertChannel.send(embed);
+                    alertChannel.send({embeds : [embed]});
                 }
             }
         }

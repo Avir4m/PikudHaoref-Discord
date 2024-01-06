763829dd583d27d507bee4a31e71303e33fb110a
unknown <avir4m@example.com>
2024-01-06 12:10:08 +0200

    removed permission checking

--- README.md
+++ README.md
@@ -2,4 +2,4 @@
 
 Sends alerts in real-time (Pikud Haoref Alerts)
 
-[Invite](https://discord.com/api/oauth2/authorize?client_id=1173211346410668083&permissions=826781206528&scope=applications.commands+bot)
+[Invite](https://discord.com/api/oauth2/authorize?client_id=1173211346410668083&permissions=2147503104&scope=applications.commands+bot)
--- src/alert.js
+++ src/alert.js
@@ -33,26 +33,24 @@ const getAlerts = async (client) => {
         const allGuilds = await GuildSettings.find();
 
         for (const guildSetting of allGuilds) {
-            const alertChannel = client.channels.cache.get(guildSetting.alertChannelId);
-            if (alertChannel || alertChannel.permissionsFor(client.user)?.has('SEND_MESSAGES')) {
-                getActiveAlert(async (err, alert) => {
-                    if (err) {
-                        console.error('Retrieving active alert failed: ', err);
-                    } else {
-                        if (alertChannel && alert.type !== 'none') {
-                            const embed = new EmbedBuilder()
-                                .setTitle(`התראות בזמן אמת`)
-                                .setColor(`#a60b00`)
-                                .setFields(
-                                    { name: `סוג התראה`, value: `${translations[alert.type]}` },
-                                    { name: `ערים`, value: `${alert.cities}` },
-                                    { name: `הוראות פיקוד העורף`, value: `${alert.instructions}` },
-                                );
-                            alertChannel.send({ embeds: [embed] });
-                        }
+            const alertChannel = client.channels.cache.get(guildSetting.alertChannelId)
+            getActiveAlert(async (err, alert) => {
+                if (err) {
+                    console.error('Retrieving active alert failed: ', err);
+                } else {
+                    if (alertChannel && alert.type !== 'none') {
+                        const embed = new EmbedBuilder()
+                            .setTitle(`התראות בזמן אמת`)
+                            .setColor(`#a60b00`)
+                            .setFields(
+                                { name: `סוג התראה`, value: `${translations[alert.type]}` },
+                                { name: `ערים`, value: `${alert.cities}` },
+                                { name: `הוראות פיקוד העורף`, value: `${alert.instructions}` },
+                            );
+                        alertChannel.send({ embeds: [embed] });
                     }
-                }, options);
-            }
+                }
+            }, options);
         }
 
         setTimeout(() => getAlerts(client), 5000);
@@ -79,10 +77,10 @@ const setAlertChannelCommand = async (interaction) => {
 
     try {
         await setAlertChannel(guildId, channelId);
-        interaction.reply(`Alert channel set to <#${channelId}>.`);
+        interaction.reply({ content: `Alert channel set to <#${channelId}>.`, ephemeral: true });
     } catch (error) {
         console.error('Error setting alert channel:', error);
-        interaction.reply('Failed to set the alert channel. Please try again.');
+        interaction.reply({ content: 'Failed to set the alert channel. Please try again.', ephemeral: true });
     }
 };
 
